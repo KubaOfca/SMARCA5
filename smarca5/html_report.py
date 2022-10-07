fc1111c8dbe8b36fe9a375f4af9f9434d006f208
unknown <b.kuba.13@gmail.com>
2022-10-07 20:18:42 +0200

    add bs4 obj generator

--- smarca5/html_report.py
+++ smarca5/html_report.py
@@ -2,19 +2,21 @@ from bs4 import BeautifulSoup
 from main import load_file
 
 SIZE_OF_THE_TOP_SPACE_BETWEEN_THE_LINES = 3
-class HtmlRaport:
-    def __int__(self):
-        self.soup = soup_obj_generator()
-        title = ""
-        display_config = {
-            "current_first_index_of_protein_sequence": 0,
-            "protein_line_height": -SIZE_OF_THE_TOP_SPACE_BETWEEN_THE_LINES,
-            "peptides_already_displayed": {},
-            "list_of_element_to_transfer": [],
-            "div_center": self.soup.find("div", {"class": "center"}),
-        }
 
-class RaportPage
+
+class HtmlReport:
+    def __int__(self, interline, text_left_offset, line_length):
+        self.soup = soup_obj_generator()
+        self.interline = interline
+        self.text_left_offset = text_left_offset
+        self.line_length = line_length
+        self.protein_seq_color = ""
+
+
+class ReportPage(HtmlReport):
+    pass
+
+
 def soup_obj_generator():
     with open(load_file(r"html_files\index.html"), "r") as html:
         soup = BeautifulSoup(html, "html.parser")
